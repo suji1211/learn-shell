94e6469df23b378be2208f2c80d14087f1606e32
suji <borra.sujani@gmail.com>
2023-04-20 14:26:01 -0400

    path changes

--- 04-functions.sh
+++ 04-functions.sh
@@ -7,3 +7,14 @@ function_name() {
 
   #call function
   function_name
+
+  ##we can send input to the functions and we can access with special variables $1 -$n,$*.$#
+
+  function_name1() {
+    echo First Argument = $1
+     echo Second Argument = $2
+     echo All arguments = $*
+     echo No of arguments = $#
+  }
+
+  function_name1 123 xyz
